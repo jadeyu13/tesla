63add68de17ee9eb3ec1eda3c38f1c253f73f22a
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2022-01-05 20:30:12 -0800

    add project path to src

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,85 +1,77 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 // import Fade from 'react-reveal/Fade';
 
-function Section({title, description, leftBtnText, backgroundImg}) {
-    return (
-        <Wrap bgImage={backgroundImg}>
+function Section({ title, description, leftBtnText, backgroundImg }) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      {/* <Fade bottom> */}
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      {/* </Fade> */}
 
-            {/* <Fade bottom> */}
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-
-        </ItemText>
-            {/* </Fade> */}
-       
-        <Buttons>
+      <Buttons>
         <ButtonGroup>
-               <LeftButton>{leftBtnText}</LeftButton>
-               {/* <RightButton></RightButton> */}
-           </ButtonGroup>
-           <DownArrow src="/images/down-arrow.svg" /> </Buttons>
-           
-            
-        </Wrap>
-    )
+          <LeftButton>{leftBtnText}</LeftButton>
+          {/* <RightButton></RightButton> */}
+        </ButtonGroup>
+        <DownArrow src="/tesla/images/down-arrow.svg" />{' '}
+      </Buttons>
+    </Wrap>
+  );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-width: 100vw;
-height: 100vh;
-background-position: center;
-background-size: cover;
-background-repeat:no-repeat;
-background-image: url('/images/model-s.jpg');
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-background-image: ${props => `url("/images/${props.bgImage}")`}
+  width: 100vw;
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
 
-`
-
-const ItemText =styled.div`
-padding-top: 15vh;
-text-align:center;
-
-`
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
 
 const ButtonGroup = styled.div`
-display: flex;
-justify-content: center; 
-@media(max-width: 768px){
+  display: flex;
+  justify-content: center;
+  @media (max-width: 768px) {
     flex-direction: column;
-}
-
-
-`
+  }
+`;
 const LeftButton = styled.div`
-background-color: rgba(23, 26, 32, 0.8);
-height: 40px;
-width: 256px;
-color: white;
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius: 100px;
-opacity: .85;
-text-transform: uppercase;
-font-size: 12px;
-cursor: pointer;
-margin: 8px
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 
 /* const RightButton = styled(LeftButton) */
 
 const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
 
-height: 40px;
-animation: animateDown infinite 1.5s;
-overflow-x: hidden;
-`
-
-const Buttons = styled.div``
+const Buttons = styled.div``;
